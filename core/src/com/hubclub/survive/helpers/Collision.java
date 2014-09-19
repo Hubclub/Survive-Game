7d4ada743e0ae9351c924bb4b744c07bb6a81370
Tarnacop <mrtarnacop@gmail.com>
2014-09-19 16:14:49 +0300

    Seventh commit
    Commit

--- core/src/com/hubclub/survive/helpers/Collision.java
+++ core/src/com/hubclub/survive/helpers/Collision.java
@@ -1,8 +1,11 @@
 package com.hubclub.survive.helpers;
 
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+
 public class Collision {
 	
-	private Array<Rectangle>;
+	private Array<Rectangle> hitBoxes; 
 	
 	
 }
