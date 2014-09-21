2fedcf989128ec438c4b4bd19643ddebc5e43e85
damianr13 <damian.robert.andrei@gmail.com>
2014-09-21 14:04:00 +0300

    Create Constants.java

---
+++ core/src/com/hubclub/survive/Constants.java
@@ -0,0 +1,13 @@
+package com.hubclub.survive;
+
+import com.badlogic.gdx.Gdx;
+
+public class Constants {
+	
+	// I use this class because enum
+	// is fucked up in java
+	
+	public static final float WIDTH_SCALE = (float)(Gdx.graphics.getWidth()) / 480;
+	public static final float HEIGHT_SCALE = (float)(Gdx.graphics.getHeight()) / 760;
+	
+}// END OF Constants CLASS
